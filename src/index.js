72622684e3a67bf80f0d1d294e0ecbfd339561fc
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-19 23:57:04 +0200

    S46L714

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { legacy_createStore, applyMiddleware, compose } from 'redux';
+import reducers from "./reducers";
 
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = legacy_createStore(
+    reducers,
+    composeEnhancer(applyMiddleware())
+    );
 
 const rootEelement = document.querySelector('#root');
 
 const root = createRoot(rootEelement);
 
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
